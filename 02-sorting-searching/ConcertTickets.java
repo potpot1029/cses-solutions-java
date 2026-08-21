e84375f2046c4c4633e9a373b36ca05947ab00d4
potpot1029 <chaukinyi1029@gmail.com>
2026-08-21 23:16:30 +0800

    impl(sorting and searching): Concert Tickets

---
+++ 02-sorting-searching/ConcertTickets.java
@@ -0,0 +1,126 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class ConcertTickets {
+  public static void main(String[] args) throws Exception {
+    FastIO io = new FastIO();
+
+    int n = io.nextInt(), m = io.nextInt();
+
+    NavigableMap<Integer, Integer> tickets = new TreeMap<>();
+    for (int i = 0; i < n; i++) {
+      int price = io.nextInt();
+
+      if (tickets.containsKey(price)) tickets.put(price, tickets.get(price) + 1);
+      else tickets.put(price, 1);
+    }
+    for (int i = 0; i < m; i++) {
+      int mxPrice = io.nextInt();
+      Map.Entry<Integer, Integer> foundTicket = tickets.lowerEntry(mxPrice + 1);
+
+      if (foundTicket != null) {
+        io.println(foundTicket.getKey());
+        if (foundTicket.getValue() == 1) tickets.remove(foundTicket.getKey());
+        else tickets.replace(foundTicket.getKey(), foundTicket.getValue() - 1);
+      } else {
+        io.println(-1);
+      }
+    }
+
+    io.close();
+  }
+
+  // from https://usaco.guide/general/fast-io?lang=java
+  static class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+      this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+      super(o);
+      stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+      super(new FileWriter(o));
+      stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+      if (numChars == -1) {
+        throw new InputMismatchException();
+      }
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars == -1) {
+          return -1; // end of file
+        }
+      }
+      return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = nextByte();
+      } while (c > ' ');
+      return res.toString();
+    }
+
+    public int nextInt() { // nextLong() would be implemented similarly
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = nextByte();
+      }
+
+      int res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res = 10 * res + c - '0';
+        c = nextByte();
+      } while (c > ' ');
+      return res * sgn;
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
