8bc58565c2ddd17b579b97c4663e7f64afb3ae85
potpot1029 <chaukinyi1029@gmail.com>
2026-08-27 23:34:05 +0800

    impl(sorting and searching): Distinct Values Subarrays, Distinct Values Subsequences, Josephus Problem I

---
+++ 02-sorting-searching/DistinctValuesSubarrays.java
@@ -0,0 +1,144 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+
+public class DistinctValuesSubarrays {
+  public static void main(String[] args) throws Exception {
+    FastIO io = new FastIO();
+
+    int n = io.nextInt();
+    int[] x = new int[n];
+    for (int i = 0; i < n; i++) {
+      x[i] = io.nextInt();
+    }
+
+    int[] prevIdentical = new int[n];
+    HashMap<Integer, Integer> num2LastIdx = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      prevIdentical[i] = num2LastIdx.getOrDefault(x[i], -1);
+      num2LastIdx.put(x[i], i);
+    }
+
+    long ans = 0;
+    int start = 0, end = 1, prevEnd = -1;
+    while (end < n && start < end) {
+      while (end < n && prevIdentical[end] < start) {
+        end++;
+      }
+
+      long rangeLen = (long) end - start;
+      ans += (rangeLen + 1) * rangeLen / 2;
+      if (start < prevEnd) {
+        long overlapRangeLen = (long) prevEnd - start;
+        ans -= (overlapRangeLen + 1) * overlapRangeLen / 2;
+      }
+
+      prevEnd = end;
+      if (end < n) {
+        start = prevIdentical[end] + 1;
+        end = start + 1;
+      }
+    }
+    long rangeLen = (long) end - start;
+    ans += (rangeLen + 1) * rangeLen / 2;
+    if (start < prevEnd) {
+      long overlapRangeLen = (long) prevEnd - start;
+      ans -= (overlapRangeLen + 1) * overlapRangeLen / 2;
+    }
+
+    io.println(ans);
+
+    io.close();
+  }
+
+  // from https://usaco.guide/general/fast-io?lang=java
+  static class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+      this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+      super(o);
+      stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+      super(new FileWriter(o));
+      stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+      if (numChars == -1) {
+        throw new InputMismatchException();
+      }
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars == -1) {
+          return -1; // end of file
+        }
+      }
+      return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = nextByte();
+      } while (c > ' ');
+      return res.toString();
+    }
+
+    public int nextInt() { // nextLong() would be implemented similarly
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = nextByte();
+      }
+
+      int res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res = 10 * res + c - '0';
+        c = nextByte();
+      } while (c > ' ');
+      return res * sgn;
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
