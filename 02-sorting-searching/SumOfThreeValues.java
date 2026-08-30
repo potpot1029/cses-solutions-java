cd084c0a3cf09a800b6793bcc71adc1273c7f9bf
potpot1029 <chaukinyi1029@gmail.com>
2026-08-30 22:49:52 +0800

    impl(sorting and searching): Reading Books, Sum of Three Values, Sum of Four Values

---
+++ 02-sorting-searching/SumOfThreeValues.java
@@ -0,0 +1,152 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+
+public class SumOfThreeValues {
+  public static void main(String[] args) {
+    FastIO io = new FastIO();
+
+    int n = io.nextInt();
+    long target = io.nextLong();
+    int[][] a = new int[n][2];
+    for (int i = 0; i < n; i++) {
+      a[i][0] = io.nextInt();
+      a[i][1] = i + 1;
+    }
+
+    Arrays.sort(a, Comparator.comparingInt((int[] b) -> b[0]));
+
+    for (int i = 0; i < n - 2; i++) {
+      long newTarget = target - a[i][0];
+      int left = i + 1, right = n - 1;
+      while (left < right) {
+        long twoSum = a[left][0] + a[right][0];
+        if (newTarget == twoSum) {
+          io.println(a[i][1] + " " + a[left][1] + " " + a[right][1]);
+          io.close();
+          return;
+        } else if (newTarget < twoSum) right--;
+        else left++;
+      }
+    }
+
+    io.println("IMPOSSIBLE");
+
+    io.close();
+  }
+
+  // from https://usaco.guide/general/fast-io?lang=java
+  static class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+      this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+      super(o);
+      stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+      super(new FileWriter(o));
+      stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+      if (numChars == -1) {
+        throw new InputMismatchException();
+      }
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars == -1) {
+          return -1; // end of file
+        }
+      }
+      return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = nextByte();
+      } while (c > ' ');
+      return res.toString();
+    }
+
+    public int nextInt() { // nextLong() would be implemented similarly
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = nextByte();
+      }
+
+      int res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res = 10 * res + c - '0';
+        c = nextByte();
+      } while (c > ' ');
+      return res * sgn;
+    }
+
+    public long nextLong() { // nextLong() would be implemented similarly
+      long c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      long sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = nextByte();
+      }
+
+      long res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res = 10 * res + c - '0';
+        c = nextByte();
+      } while (c > ' ');
+      return res * sgn;
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
