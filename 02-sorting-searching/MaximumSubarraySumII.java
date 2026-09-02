0a1f64119ff21dec7809f165753283d823cb978d
potpot1029 <chaukinyi1029@gmail.com>
2026-09-02 23:42:42 +0800

    impl(sorting and searching): Maximum Subarray Sum II

---
+++ 02-sorting-searching/MaximumSubarraySumII.java
@@ -0,0 +1,126 @@
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
+import java.util.TreeMap;
+
+public class MaximumSubarraySumII {
+  public static void main(String[] args) {
+    FastIO io = new FastIO();
+
+    int n = io.nextInt(), mnSize = io.nextInt(), mxSize = io.nextInt();
+    int[] x = new int[n];
+    long[] prefixSum = new long[n + 1];
+    for (int i = 0; i < n; i++) {
+      x[i] = io.nextInt();
+      prefixSum[i + 1] = prefixSum[i] + x[i];
+    }
+
+    long mxSum = Long.MIN_VALUE;
+    TreeMap<Long, Integer> prefixCandidates = new TreeMap<>();
+
+    for (int i = mnSize; i <= n; i++) {
+      if (i > mxSize) {
+        long outOfRangePrefix = prefixSum[i - mxSize - 1];
+        if (prefixCandidates.get(outOfRangePrefix) == 1) prefixCandidates.remove(outOfRangePrefix);
+        else prefixCandidates.put(outOfRangePrefix, prefixCandidates.get(outOfRangePrefix) - 1);
+      }
+      long newPrefixSum = prefixSum[i - mnSize];
+      prefixCandidates.put(newPrefixSum, prefixCandidates.getOrDefault(newPrefixSum, 0) + 1);
+      mxSum = Math.max(mxSum, prefixSum[i] - (long) prefixCandidates.firstKey());
+    }
+
+    io.println(mxSum);
+
+    io.close();
+  }
+
+  // from https://usaco.guide/general/fast-io?lang=java
+  static class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+      this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+      super(o);
+      stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+      super(new FileWriter(o));
+      stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+      if (numChars == -1) {
+        throw new InputMismatchException();
+      }
+      if (curChar >= numChars) {
+        curChar = 0;
+        try {
+          numChars = stream.read(buf);
+        } catch (IOException e) {
+          throw new InputMismatchException();
+        }
+        if (numChars == -1) {
+          return -1; // end of file
+        }
+      }
+      return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      StringBuilder res = new StringBuilder();
+      do {
+        res.appendCodePoint(c);
+        c = nextByte();
+      } while (c > ' ');
+      return res.toString();
+    }
+
+    public int nextInt() { // nextLong() would be implemented similarly
+      int c;
+      do {
+        c = nextByte();
+      } while (c <= ' ');
+
+      int sgn = 1;
+      if (c == '-') {
+        sgn = -1;
+        c = nextByte();
+      }
+
+      int res = 0;
+      do {
+        if (c < '0' || c > '9') {
+          throw new InputMismatchException();
+        }
+        res = 10 * res + c - '0';
+        c = nextByte();
+      } while (c > ' ');
+      return res * sgn;
+    }
+
+    public double nextDouble() {
+      return Double.parseDouble(next());
+    }
+  }
+}
